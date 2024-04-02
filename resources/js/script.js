cf52fc2f2b348e0457496222978082e85eb015f9
Valerio-Catellani <valeriocatellani@gmail.com>
2024-04-02 15:54:10 +0200

    created animation fro chekbox

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,13 +1,40 @@
 import * as hype from "./hypeUtility.js";
-import {post} from "./data.js";
+import { post } from "./data.js";
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
     data() {
         return {
-                post:post,
+            list: '',
+            post: post,
         }
+    },
+    methods: {
+        selectPost(id) {
+            let element;
+            this.post.forEach(el => {
+                if (el.id === id) {
+                    element = el
+                }
+            })
+            element.done = !element.done
+        },
+        removePost(id) {
+            const index = this.post.findIndex((el) => {
+                return el.id === id;
+            })
+            if (index !== -1) {
+                this.post.splice(index, 1)
+            }
+        },
+    },
+
+    computed: {
+
     }
 
 }).mount('#app')
+
+
+
